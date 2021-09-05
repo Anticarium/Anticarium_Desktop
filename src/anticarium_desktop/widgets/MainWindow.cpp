06571623ef5beb9493a0552a3ec2aa56aa25d8fa
Onii-Chaan <martinnc@inbox.lv>
2021-09-05 12:16:48 +0300

    Returned renamed mainwindow

---
+++ src/anticarium_desktop/widgets/MainWindow.cpp
@@ -0,0 +1,39 @@
+#include "MainWindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+
+MainWindow::MainWindow(QWidget* parent)
+: QMainWindow(parent), ui(new Ui::MainWindow),
+  settings(new QSettings(QSettings::Format::IniFormat, QSettings::Scope::SystemScope, QCoreApplication::organizationName(), QCoreApplication::applicationName())) {
+    // instanciate the singleton
+    jttp = JTTP::GetInstance(parent, settings);
+
+    ui->setupUi(this);
+
+    // updates displayed values
+    //    connect(jttp, &JTTP::updateSensorDisplay, homeTab, &HomeTab::updateSensorDisplay);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+void MainWindow::onSetupData(const shared_types::TerrariumData& terrariumData) {
+    ui->modeLabel->setText(terrariumData.getCurrentRegime());
+    onControlUpdate(terrariumData.getControl());
+}
+
+void MainWindow::onControlUpdate(const shared_types::Control& control) {
+    ui->windSlider->setValue(control.getWindPercentage());
+    ui->lightSlider->setValue(control.getLightPercentage());
+    ui->heatToggle->setChecked(control.isHeating());
+    ui->rainToggle->setChecked(control.isRaining());
+    ui->autoToggle->setChecked(control.isAuto());
+}
+
+void MainWindow::onSensorDataUpdate(const shared_types::SensorData& sensorData) {
+    ui->temperatureValue->display(sensorData.getTemperature());
+    ui->humidityValue->display(sensorData.getHumidity());
+    ui->moistureValue->display(sensorData.getMoisture());
+}
---
+++ src/anticarium_desktop/widgets/MainWindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "Jttp.h"
+#include <MainWindowManager.h>
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+  public:
+    MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+  public slots:
+    void onSetupData(const shared_types::TerrariumData& terrariumData);
+    void onControlUpdate(const shared_types::Control& control);
+    void onSensorDataUpdate(const shared_types::SensorData& sensorData);
+
+  private:
+    Ui::MainWindow* ui;
+    QSettings* settings = nullptr;
+    // makes HTTP calls to the server and processes received data
+    JTTP* jttp                 = nullptr;
+    MainWindowManager* manager = nullptr;
+};
+#endif // MAINWINDOW_H
