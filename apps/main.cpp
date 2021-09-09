3310a05f504327709ecefaf0513b8fb98a8e40ae
Onii-Chaan <martinnc@inbox.lv>
2021-09-09 17:59:30 +0300

    Removed unused code from main

--- apps/main.cpp
+++ apps/main.cpp
@@ -2,17 +2,8 @@
 #include <QApplication>
 #include <widgets/MainWindow.h>
 
-Q_DECLARE_METATYPE(shared_types::SensorData);
-
-
 int main(int argc, char* argv[]) {
-    qRegisterMetaType<shared_types::SensorData>("shared_types::SensorData");
-
-
     QApplication a(argc, argv);
-    // for Anticarium.ini
-    QCoreApplication::setOrganizationName("Team_7");
-    QCoreApplication::setApplicationName("Anticarium");
 
     JTTP::instance(QCoreApplication::instance());
 
