51d1803c829d12d07dd1f7b68cd5b22a6d11e3d7
MartinsSmirnovs <37780997+MartinsSmirnovs@users.noreply.github.com>
2021-10-29 10:59:15 +0300

    Display regimes

--- src/anticarium_desktop/widgets/DisplayRegimes.cpp
+++ src/anticarium_desktop/widgets/DisplayRegimes.cpp
@@ -3,6 +3,9 @@
 
 DisplayRegimes::DisplayRegimes(QWidget* parent) : QDialog(parent), ui(new Ui::DisplayRegimes) {
     ui->setupUi(this);
+
+    setWindowTitle("Saved Regimes");
+
     connect(ui->regimesButtonBox, &QDialogButtonBox::accepted, this, &DisplayRegimes::accept);
     connect(ui->regimesButtonBox, &QDialogButtonBox::rejected, this, &DisplayRegimes::reject);
 
@@ -11,17 +14,57 @@ DisplayRegimes::DisplayRegimes(QWidget* parent) : QDialog(parent), ui(new Ui::Di
     manager = new DisplayRegimesManager(this);
     connect(manager, &DisplayRegimesManager::displayDataEvent, this, &DisplayRegimes::onDisplayData);
     manager->initialize();
+
+    adjustSize();
 }
 
 DisplayRegimes::~DisplayRegimes() {
     delete ui;
 }
 
-#include <QDebug>
 void DisplayRegimes::onDisplayData(const shared_types::SavedRegimes& savedRegimes) {
-    qDebug() << "DisplayRegimes::onDisplayData";
+    ui->table->setRowCount(0);
+    std::vector<shared_types::Regime> regimesList = savedRegimes.getSavedRegimes();
+
+    int regimesListSize = regimesList.size();
+    ui->table->setRowCount(regimesListSize);
+    for (int i = 0; i < regimesListSize; ++i) {
+        QTableWidgetItem* item = nullptr;
+
+        item = new QTableWidgetItem(regimesList[i].getRegimeName().getName(), Qt::DisplayRole);
+        ui->table->setItem(i, 0, item);
+
+        item = new QTableWidgetItem(QString::number(regimesList[i].getRegimeValue().getTemperature()), Qt::DisplayRole);
+        ui->table->setItem(i, 1, item);
+
+        item = new QTableWidgetItem(QString::number(regimesList[i].getRegimeValue().getMoisture()), Qt::DisplayRole);
+        ui->table->setItem(i, 2, item);
+    }
 }
 
 void DisplayRegimes::initializeTableHeader() {
-    QHeaderView* tableHeader = ui->tableView->horizontalHeader();
+    ui->table->setColumnCount(3);
+
+    QTableWidgetItem* columnName = nullptr;
+
+    columnName = new QTableWidgetItem("Name", Qt::DisplayRole);
+    ui->table->setHorizontalHeaderItem(0, columnName);
+
+    columnName = new QTableWidgetItem("Temperature", Qt::DisplayRole);
+    ui->table->setHorizontalHeaderItem(1, columnName);
+
+    columnName = new QTableWidgetItem("Moisture", Qt::DisplayRole);
+    ui->table->setHorizontalHeaderItem(2, columnName);
+
+    // Stretch rows
+    ui->table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    // Hide vertical header
+    ui->table->verticalHeader()->hide();
+
+    // Select whole row on click
+    ui->table->setSelectionBehavior(QAbstractItemView::SelectRows);
+
+    // Disable edit on double click
+    ui->table->setEditTriggers(QAbstractItemView::NoEditTriggers);
 }
